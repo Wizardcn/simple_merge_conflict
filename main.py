af705954576bd9d8b863930e64db000d5ce73318
Wizardcn <supawat125947@gmail.com>
2023-06-24 23:37:23 +0700

    v3

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ def calculate_sum(a, b):
 def calculate_product(a, b):
     return a * b
 
+def print_st(a):
+	print(a)
+
 def main():
     x = 5
     y = 10
     result = calculate_sum(x, y)
-    print("Sum:", result)
+    print("Summation:", result)
 
 if __name__ == "__main__":
     main()
